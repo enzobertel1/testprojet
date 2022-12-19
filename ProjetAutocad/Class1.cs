4129aa19a3d2a98896d5e98f294715797175813b
unknown <enzobertel@gmail.com>
2022-12-19 13:35:29 +0100

    first commit

---
+++ ProjetAutocad/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+using AcAp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+
+namespace ProjetAutocad
+{
+    internal class Initilization :IExtensionApplication
+    {
+
+        #region Commands
+        [CommandMethod("MaCommande1")]
+        public void cmd1()
+        {
+            var doc = AcAp.DocumentManager.MdiActiveDocument;
+
+            var db = doc.Database;
+            var ed = doc.Editor;
+
+            ed.WriteMessage("\nI have created my first command");
+
+            using (var tr = db.TransactionManager.StartTransaction())
+            {
+                BlockTable blkbl = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord msBlRec = tr.GetObject(blkbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                Point3d pt1 = new Point3d(0, 0,0);
+                Point3d pt2 = new Point3d(10, 10,0);
+
+                Line ligneObj = new Line(pt1, pt2); // Créé une ligne entre les deux points
+                msBlRec.AppendEntity(ligneObj); 
+                tr.AddNewlyCreatedDBObject(ligneObj, true); // trace la ligne avec les deux points
+
+                tr.Commit();
+            }
+
+        }
+        #endregion
+
+
+        void IExtensionApplication.Initialize()
+        {
+
+        }
+
+        void IExtensionApplication.Terminate()
+        {
+
+        }
+        //endregion
+    }
+}
---
+++ ProjetAutocad/bin/Debug/net5.0-windows/Dessin1.dwl
@@ -0,0 +1,3 @@
+Enzo_Bertel
+KADRAN-INNO-002 
+jeudi 29 septembre 2022  10:49:55
